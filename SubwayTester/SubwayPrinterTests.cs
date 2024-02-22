479b938246b81c422eff965f375daee8821734dd
juliarutkowska <julia.rutkowska189@gmail.com>
2024-02-22 10:18:35 +0100

    test fixed

--- SubwayTester/SubwayPrinterTests.cs
+++ SubwayTester/SubwayPrinterTests.cs
@@ -1,6 +1,7 @@
 using Chapter10;
 using System;
 using System.Collections.Generic;
+using System.Text;
 using AutoFixture.Xunit2;
 using Xunit;
 
@@ -19,30 +20,31 @@ public class SubwayPrinterTests
 
         outputStream.Seek(0, SeekOrigin.Begin);
         using var reader = new StreamReader(outputStream);
-        var output = reader.ReadToEnd();
         
         for (var i = 0 ; i< connections.Count;++i)
         {
+            var stringBuilder = new StringBuilder();
+            
             if (i == 0)
             {
-                Assert.Contains($"Start out at {connections[i].Station1.Name}", output);
-                Assert.Contains($"Get on the {connections[i].LineName} heading towards {connections[i].Station2.Name}.", output);
+                stringBuilder.Append($"Start out at {connections[i].Station1.Name}");
+                stringBuilder.Append($"Get on the {connections[i].LineName} heading towards {connections[i].Station2.Name}.");
                 continue;
             }
 
             if (i == connections.Count - 1)
             {
-                Assert.Contains($"Get off at {connections[i].Station2.Name} and enjoy yourself!", output);
+                stringBuilder.Append($"Get off at {connections[i].Station2.Name} and enjoy yourself!");
             }
             else
             {
-                Assert.Contains($"Switch over to the {connections[i].LineName}, heading towards {connections[i].Station2.Name}.", output);
+                stringBuilder.Append($"Switch over to the {connections[i].LineName}, heading towards {connections[i].Station2.Name}.");
             }
 
             if (i > 0 && i < connections.Count - 1)
             {
-                Assert.Contains($"Continue past {connections[i - 1].Station2.Name}...", output);
-                Assert.Contains($"When you get to {connections[i].Station2.Name}, get off the {connections[i].LineName}.", output);
+                stringBuilder.Append($"Continue past {connections[i].Station1.Name}...");
+                stringBuilder.Append($"When you get to {connections[i].Station2.Name}, get off the {connections[i].LineName}.");
             }
         }
     }
