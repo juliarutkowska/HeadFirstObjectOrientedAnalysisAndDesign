a5b4cac67ca0e3b1b5f53edf4e29f83c1537a4d1
juliarutkowska <julia.rutkowska189@gmail.com>
2024-02-23 09:39:47 +0100

    tests fixed

--- SubwayTester/SubwayPrinterTests.cs
+++ SubwayTester/SubwayPrinterTests.cs
@@ -1,9 +1,6 @@
 using Chapter10;
-using System;
-using System.Collections.Generic;
 using System.Text;
 using AutoFixture.Xunit2;
-using Xunit;
 
 namespace SubwayTester;
 
@@ -11,41 +8,70 @@ public class SubwayPrinterTests
 {
     [Theory]
     [AutoData]
-    public void PrintDirections_PrintsDirectionsCorrectly(List<Connection>connections)
+    public void PrintDirections_PrintsDirectionsCorrectly(List<Connection> connections)
     {
         var outputStream = new MemoryStream();
         var subwayPrinter = new SubwayPrinter(outputStream);
-        
+
         subwayPrinter.PrintDirections(connections);
 
         outputStream.Seek(0, SeekOrigin.Begin);
         using var reader = new StreamReader(outputStream);
-        
-        for (var i = 0 ; i< connections.Count;++i)
+        var output = reader.ReadToEnd();
+        var stringBuilder = new StringBuilder();
+        for (var i = 0; i < connections.Count; ++i)
         {
-            var stringBuilder = new StringBuilder();
-            
             if (i == 0)
             {
-                stringBuilder.Append($"Start out at {connections[i].Station1.Name}");
-                stringBuilder.Append($"Get on the {connections[i].LineName} heading towards {connections[i].Station2.Name}.");
+                stringBuilder.AppendLine($"Start out at {connections[i].Station1.Name}.");
+                stringBuilder.AppendLine($"Get on the {connections[i].LineName} heading towards {connections[i].Station2.Name}.");
                 continue;
             }
-
-            if (i == connections.Count - 1)
+            
+            if (connections[i].LineName.Equals(connections[i-1].LineName))
             {
-                stringBuilder.Append($"Get off at {connections[i].Station2.Name} and enjoy yourself!");
+                stringBuilder.AppendLine($"Continue past {connections[i].Station1.Name}...");
             }
             else
             {
-                stringBuilder.Append($"Switch over to the {connections[i].LineName}, heading towards {connections[i].Station2.Name}.");
+                stringBuilder.AppendLine($"When you get to {connections[i].Station1.Name} get off the {connections[i-1].LineName}.");
+                stringBuilder.AppendLine($"Switch over to the {connections[i].LineName}, heading towards {connections[i].Station2.Name}.");
             }
-
-            if (i > 0 && i < connections.Count - 1)
+            
+            if (i == connections.Count - 1)
             {
-                stringBuilder.Append($"Continue past {connections[i].Station1.Name}...");
-                stringBuilder.Append($"When you get to {connections[i].Station2.Name}, get off the {connections[i].LineName}.");
+                stringBuilder.AppendLine($"Get off at {connections[i].Station2.Name} and enjoy yourself!");
             }
         }
+        Assert.Equal(stringBuilder.ToString(), output);
     }
+
+    //     for (var i = 0 ; i < connections.Count;++i)
+    //     {
+    //         if (i == 0)
+    //         {
+    //             stringBuilder.AppendLine($"Start out at {connections[i].Station1.Name}.");
+    //             stringBuilder.AppendLine($"Get on the {connections[i].LineName} heading towards {connections[i].Station2.Name}.");
+    //             continue;
+    //         }
+    //
+    //         if (i > 0 && i < connections.Count - 1)
+    //         {
+    //             stringBuilder.AppendLine($"Continue past {connections[i].Station1.Name}...");
+    //             
+    //         }
+    //         else
+    //         {
+    //             stringBuilder.AppendLine($"When you get to {connections[i].Station2.Name} get off the {connections[i].LineName}.");
+    //             stringBuilder.AppendLine($"Switch over to the {connections[i].LineName}, heading towards {connections[i].Station2.Name}.");
+    //         }
+    //         
+    //         if (i == connections.Count - 1)
+    //         {
+    //             stringBuilder.AppendLine($"Get off at {connections[i].Station2.Name} and enjoy yourself!");
+    //         }
+    //         
+    //     }
+    //     Assert.Equal(stringBuilder.ToString(), output);
+    // }
 }
